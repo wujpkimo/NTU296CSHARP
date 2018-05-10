5c8cd69b52122004772e18401ad78d644f6f09e3
JasonWu(吳志平)-Notebook <wujpkimo@gmail.com>
2018-05-10 22:07:55 +0800

    2018-05-10 Jason 新增猜數字出題器

---
+++ RandomGen/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RandomGen
+{
+    partial class 猜數字出題器
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(25, 28);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(3);
+            this.lbl1.MinimumSize = new System.Drawing.Size(50, 50);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(50, 50);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "0";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(81, 28);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(3);
+            this.lbl2.MinimumSize = new System.Drawing.Size(50, 50);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(50, 50);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "0";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(137, 28);
+            this.lbl3.Margin = new System.Windows.Forms.Padding(3);
+            this.lbl3.MinimumSize = new System.Drawing.Size(50, 50);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(50, 50);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "0";
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(193, 28);
+            this.lbl4.Margin = new System.Windows.Forms.Padding(3);
+            this.lbl4.MinimumSize = new System.Drawing.Size(50, 50);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(50, 50);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "0";
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(33, 84);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(210, 46);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // 猜數字出題器
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 159);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "猜數字出題器";
+            this.Text = "猜數字出題器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lbl2;
+        public System.Windows.Forms.Label lbl3;
+        public System.Windows.Forms.Label lbl4;
+        public System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ RandomGen/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RandomGen
+{
+    public partial class 猜數字出題器 : Form
+    {
+        public 猜數字出題器()
+        {
+            InitializeComponent();
+            randomList(lbl1, lbl2, lbl3, lbl4);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            randomList(lbl1, lbl2, lbl3, lbl4);
+        }
+
+        /// <summary>
+        /// 4位不重複產生器(含更新4個LABEL控制項)
+        /// </summary>
+        /// <param name="label1"></param>
+        /// <param name="label2"></param>
+        /// <param name="label3"></param>
+        /// <param name="label4"></param>
+        public void randomList(Label label1, Label label2, Label label3, Label label4)
+        {
+            Random random = new Random();
+            //產生0-9的LIST
+            List<int> iList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //建立空LIST
+            List<int> result = new List<int>();
+            for (int i = 1; i <= 4; i++)
+            {
+                //取索引
+                int index = random.Next(0, iList.Count - 1);
+                //在結果LIST寫入值
+                result.Add(iList[index]);
+                //從0-9LIST移除已取出數值
+                iList.Remove(iList[index]);
+            }
+
+            #region 寫入畫面控制項
+
+            label1.Text = result[0].ToString();
+            label2.Text = result[1].ToString();
+            label3.Text = result[2].ToString();
+            label4.Text = result[3].ToString();
+
+            #endregion 寫入畫面控制項
+        }
+    }
+}
