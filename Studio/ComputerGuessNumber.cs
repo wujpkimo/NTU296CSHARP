e46e252b8b7d26825669f2480158e2129757f8e7
JasonWu(吳志平)-Notebook <wujpkimo@gmail.com>
2018-05-28 07:35:36 +0800

    2018-05-28 Jason Commit Studio

---
+++ Studio/ComputerGuessNumber.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studio
+{
+    internal class ComputerGuessNumber
+    {
+        //可能答案清單
+        private Guestlis guestList = new Guestlis();
+
+        //移除清單
+        private List<string> removeList = new List<string>();
+
+        private int ansCount;
+        private List<int> ans = new List<int>();
+
+        //電腦試猜答案
+        private string ansString = string.Empty;
+
+        private Dictionary<string, int> chkResult = new Dictionary<string, int> { { "a", 0 }, { "b", 0 } };
+
+        //產生0-9的LIST
+        private List<int> iList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        //判定結果
+        private string result;
+
+        //猜題次數
+        private int count = 0;
+
+        public ComputerGuessNumber()
+        {
+            count++;
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            //產生0-9的LIST
+            iList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //建立空LIST
+            for (int i = 1; i <= 4; i++)
+            {
+                //取索引
+                int index = random.Next(0, iList.Count - 1);
+                //在結果LIST寫入值
+                ans.Add(iList[index]);
+                ansString += iList[index].ToString();
+                //從0-9LIST移除已取出數值
+                iList.Remove(iList[index]);
+            }
+
+            Console.WriteLine("電腦第{0}次猜:{1}", count.ToString(), ansString);
+
+            //0A0B大量移除
+            chkResult = new Dictionary<string, int> { { "a", 0 }, { "b", 0 } };
+            if (result == "0A0B")
+            {
+                foreach (string x in guestList.ansList)
+                {
+                    foreach (char t in ansString)
+                    {
+                        if (x.IndexOf(t) >= 1)
+                            removeList.Add(x);
+                    }
+                }
+            }
+
+            foreach (string x in guestList.ansList)
+            {
+                if (result != GetChkResult(ansString, x))
+                    removeList.Add(x);
+            }
+
+            foreach (string x in removeList)
+                guestList.ansList.Remove(x);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="result">請放入前次猜題結果</param>
+        public ComputerGuessNumber(string result)
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            Console.WriteLine("結果是:{0}", result);
+
+            if (result == "4A0B")
+            {
+                Console.WriteLine("電腦在第{0}次猜中了！", count);
+            }
+            //0A0B大量移除
+            else if (result == "0A0B")
+                foreach (string x in guestList.ansList)
+                    foreach (char t in ansString)
+                        if (x.IndexOf(t) >= 1)
+                            removeList.Add(x);
+
+            if (count == 1)
+            {
+                for (int i = 1; i <= 4; i++)
+                {
+                    //取索引
+                    int index = random.Next(0, iList.Count - 1);
+                    //在結果LIST寫入值
+                    ans.Add(iList[index]);
+                    ansString += iList[index].ToString();
+                    //從0-9LIST移除已取出數值
+                    iList.Remove(iList[index]);
+                }
+            }
+            else
+            {
+                int indexList;
+
+                if (guestList.ansList.Count > 1)
+                {
+                    indexList = random.Next(0, guestList.ansList.Count - 1);
+                }
+                else
+                {
+                    indexList = 0;
+                }
+                ansString = guestList.ansList[indexList].ToString();
+            }
+
+            foreach (string x in guestList.ansList)
+            {
+                if (result != GetChkResult(ansString, x))
+                    removeList.Add(x);
+            }
+
+            foreach (string x in removeList)
+            {
+                guestList.ansList.Remove(x);
+            }
+            ansCount = guestList.ansList.Count();
+        }
+
+        private string GetChkResult(string question, string ans)
+        {
+            Dictionary<string, int> chkResult = new Dictionary<string, int> { { "a", 0 }, { "b", 0 } };
+            for (int j = 0; j < 4; j++)
+                for (int k = 0; k < 4; k++)
+                {
+                    if (question[j] == ans[k] && j == k)
+                        chkResult["a"]++;
+                    else if (question[j] == ans[k])
+                        chkResult["b"]++;
+                }
+            return string.Format("{0}A{1}B", chkResult["a"].ToString(), chkResult["b"].ToString());
+        }
+    }
+}
---
+++ Studio/GuessNumber.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Studio
+{
+    /// <summary>
+    /// 猜數字程式
+    /// </summary>
+    public class GuessNumber
+    {
+        public List<int> result = new List<int>();
+        public string resultString = string.Empty;
+
+        /// <summary>
+        /// 初始化出題
+        /// </summary>
+        public GuessNumber()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            //產生0-9的LIST
+            List<int> iList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //建立空LIST
+            for (int i = 1; i <= 4; i++)
+            {
+                //取索引
+                int index = random.Next(0, iList.Count - 1);
+                //在結果LIST寫入值
+                result.Add(iList[index]);
+                resultString += iList[index].ToString();
+                //從0-9LIST移除已取出數值
+                iList.Remove(iList[index]);
+            }
+        }
+
+        /// <summary>
+        /// 輸入並驗證答案
+        /// 含規則檢核
+        /// </summary>
+        /// <param name="Ans">答案</param>
+        public string ChkAns(string Ans)
+        {
+            Dictionary<string, int> chkResult = new Dictionary<string, int> { { "a", 0 }, { "b", 0 } };
+
+            #region 檢查答案是否合法
+
+            Match match = Regex.Match(Ans, @"^\d{4}$");
+            if (!match.Success)
+                return "答案不合法";
+            List<string> ans = new List<string>();
+            for (int i = 0; i < Ans.Length; i++)
+            {
+                ans.Add(Ans[i].ToString());
+                for (int j = i + 1; j < Ans.Length; j++)
+                {
+                    if (Ans[i] == Ans[j])
+                        return "答案不合法";
+                }
+            }
+
+            #endregion 檢查答案是否合法
+
+            #region 檢查結果
+
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                {
+                    if (result[i] == int.Parse(ans[j]) && i == j)
+                        chkResult["a"]++;
+                    else if (result[i] == int.Parse(ans[j]))
+                        chkResult["b"]++;
+                }
+
+            #endregion 檢查結果
+
+            return string.Format("{0}A{1}B", chkResult["a"].ToString(), chkResult["b"].ToString());
+        }
+    }
+}
