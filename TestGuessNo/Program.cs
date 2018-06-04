e1c058ac984c607c432064aee68a18fcd4df67de
JasonWu(吳志平)-Notebook <wujpkimo@gmail.com>
2018-06-04 10:00:49 +0800

    2018-06-04  Jason 測試用程式

---
+++ TestGuessNo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Studio;
+
+namespace TestGuessNo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string anstring;
+            GuessNumber guessNumber = new GuessNumber();
+            Console.WriteLine("電腦出題：{0}", guessNumber.resultString);
+
+            ComputerGuessNumber computerGuessNumber = new ComputerGuessNumber();
+
+            //將答案輸入產生結果
+            anstring = guessNumber.ChkAns(computerGuessNumber.ansString);
+
+            Console.WriteLine(guessNumber.ChkAns(computerGuessNumber.ansString));
+
+            while (anstring != "4A0B")
+            {
+                //猜答案
+                computerGuessNumber.Guess(anstring);
+                //將答案輸入產生結果
+                anstring = guessNumber.ChkAns(computerGuessNumber.ansString);
+                Console.WriteLine(guessNumber.ChkAns(computerGuessNumber.ansString));
+            }
+            Console.WriteLine("電腦在第{0}次猜中了！", computerGuessNumber.count.ToString());
+        }
+    }
+}
